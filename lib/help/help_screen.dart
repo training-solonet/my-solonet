3e7fe9a980b6388d0ef640134e4574ecd47bcacf
vandraaa <kevinandranugroho732@gmail.com>
2024-10-21 11:04:59 +0700

    Faq

--- lib/help/help_screen.dart
+++ lib/help/help_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mysolonet/help/faq/faq_sreen.dart';  // Import the FaqScreen here
 
 class HelpScreen extends StatelessWidget {
   const HelpScreen({Key? key}) : super(key: key);
@@ -26,7 +27,10 @@ class HelpScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               _buildHelpCard('assets/images/1.png', 'Setel Ulang Modem', 0, () {}),
-              _buildHelpCard('assets/images/2.png', 'Lihat Pertanyaan', 1, () {}),
+              _buildHelpCard('assets/images/2.png', 'Lihat Pertanyaan', 1, () {
+                // Navigate to FaqScreen when this button is pressed
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const FaqScreen()));
+              }),
               _buildHelpCard('assets/images/3.png', 'Pengaduan Layanan', 2, () {}),
               _buildHelpCard('assets/images/4.png', 'Cari SoloNet Terdekat', 3, () {}),
             ],
@@ -36,7 +40,7 @@ class HelpScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildHelpCard(String imagePath, String buttonText, int index, onPress) {
+  Widget _buildHelpCard(String imagePath, String buttonText, int index, VoidCallback onPress) {
     return Card(
       margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
       shape: RoundedRectangleBorder(
@@ -46,26 +50,20 @@ class HelpScreen extends StatelessWidget {
       child: Stack(
         children: [
           ClipRRect(
-            borderRadius: BorderRadius.circular(
-                10),
+            borderRadius: BorderRadius.circular(10),
             child: Image.asset(
               imagePath,
               height: 190,
               width: double.infinity,
-              fit: BoxFit
-                  .cover,
+              fit: BoxFit.cover,
             ),
           ),
           Positioned(
             bottom: 10,
-            left:
-                index % 2 == 0 ? 20 : null,
-            right:
-                index % 2 != 0 ? 20 : null, 
+            left: index % 2 == 0 ? 20 : null,
+            right: index % 2 != 0 ? 20 : null,
             child: ElevatedButton(
-              onPressed: () {
-                onPress;
-              },
+              onPressed: onPress,
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.white,
                 foregroundColor: Colors.black,
@@ -79,7 +77,7 @@ class HelpScreen extends StatelessWidget {
                 style: const TextStyle(
                   fontFamily: 'Poppins',
                   fontWeight: FontWeight.w600,
-                  fontSize: 12, 
+                  fontSize: 12,
                 ),
               ),
             ),
